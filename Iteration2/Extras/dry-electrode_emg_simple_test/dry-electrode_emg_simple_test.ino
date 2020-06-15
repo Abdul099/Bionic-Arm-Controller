24418034392efcfa306dd01e9e7ab569798dcd10
Abdullatif Hassan <aboudi.hassan@gmail.com>
2020-06-14 22:16:41 -0400

    further development into initial dry electrode

--- Iteration2/Extras/dry-electrode_emg_simple_test/dry-electrode_emg_simple_test.ino
+++ Iteration2/Extras/dry-electrode_emg_simple_test/dry-electrode_emg_simple_test.ino
@@ -11,41 +11,39 @@ EMGFilters myFilter;
 int sampleRate = SAMPLE_FREQ_1000HZ;
 int humFreq = NOTCH_FREQ_50HZ;
 short num;
+int reading; 
 short hold = 0;
-unsigned long timeStamp;
-unsigned long timeBudget;
 
 void setup() {
   myFilter.init(sampleRate, humFreq, true, true, true);
-
-  // open serial
   Serial.begin(115200);
-  timeBudget = 1e6 / sampleRate;
-  // micros will overflow and auto return to zero every 70 minutes
 }
 
 void loop() {
-  timeStamp = micros();
-
-  int Value = analogRead(SensorInputPin);
-  int DataAfterFilter = myFilter.update(Value);
-  int envlope = sq(DataAfterFilter);
-
-  if (envlope > 10000) {
-    envlope = 0;
+  reading = sample();
+  Serial.println(reading);
+  if (reading > 10000) {
+    reading = 0;
     num = 2;
     hold = 10;
   }
-  else if (envlope < 100) num = 0;
+  else if (reading < 100) num = 0;
   else {
     num = 1;
     hold = 20;
   }
-  Serial.println(envlope);
   LightLED(num);
-  if(hold!=0) hold-=1;
+  if(hold>0) hold-=1;
   delay(10);
 }
+
+int sample(){
+  int Value = analogRead(SensorInputPin);
+  int DataAfterFilter = myFilter.update(Value);
+  int envlope = sq(DataAfterFilter);
+  return envlope;
+}
+
 void LightLED(int number) {
   if (hold == 0 || number !=0) {
     switch (number) {
